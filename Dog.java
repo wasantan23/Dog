a5d8ab1c7da586b5f4a635041719a06b450656fb
wasantan23 <89757403+wasantan23@users.noreply.github.com>
2022-02-11 09:31:06 -0500

    11th Feb 2022

---
+++ Dog.java
@@ -0,0 +1,25 @@
+public class Dog {
+private String name; 
+private int birth;
+
+
+
+public Dog (String name, int birth) {
+this.name = name;
+this.birth = birth;
+
+}
+
+
+public void eat () {
+}
+public void sleep () {
+}
+
+
+public String toString () {
+	return ("The dogs name is" + name + ". The dogs birthday is" + birth);
+} 
+
+}
+
